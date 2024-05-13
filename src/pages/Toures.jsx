3b16ee74cc03a3c8e50da1df16306a17e5095184
Williyam Silvage Sachini Dishala <90495152+desi98@users.noreply.github.com>
2024-05-13 19:57:59 +0530

    create jsx pages

---
+++ src/pages/Toures.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import CommonSection from '../shared/CommonSection'
+
+
+
+function Toures() {
+  return (
+    <>
+    <CommonSection title ={"All Tours"}/>
+    </>
+  );
+};
+
+export default Toures
---
+++ src/shared/CommonSection.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import './common-section.css'
+import {Container, Row ,Col} from 'reactstrap'
+
+const CommonSection = ({title}) => {
+  return (
+    <section className="common_section">
+        <Container>
+            <Row>
+                <col lg ='12'>
+                    <h1>
+                        {title}
+                    </h1>
+                </col>
+            </Row>
+        </Container>
+    </section>
+
+
+
+   
+  )
+}
+
+export default CommonSection
