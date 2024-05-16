21892cb3d506ab98100b1ec67ba9b50d88a8adde
Williyam Silvage Sachini Dishala <90495152+desi98@users.noreply.github.com>
2024-05-16 20:05:49 +0530

    import files

---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+import Header from "e:/React/TravelMe/fontend/src/components/Header/Header";
+import Footer from "e:/React/TravelMe/fontend/src/components/Footer/Footer";
+
+import Router from '../../Router/Router';
+
+const Layout = () => {
+  return
+    <div>Layout</div>
+  
+}
+
+export default Layout;
