68a8a9299247a3c5df72967e93ab180904dcffeb
Williyam Silvage Sachini Dishala <90495152+desi98@users.noreply.github.com>
2024-06-07 20:58:47 +0530

    import browser router

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import "remixicon/fonts/remixicon.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import { BrowserRouter } from 'react-router-dom';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
