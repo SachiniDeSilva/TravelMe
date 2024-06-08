115429d4eeed0241bd90983e48172309e89304db
Williyam Silvage Sachini Dishala <90495152+desi98@users.noreply.github.com>
2024-06-09 01:14:10 +0530

    add images

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
-import '../styles/home.css'
-
+import '../styles/home.css';
+import { Container,Row,Col, Form } from 'reactstrap';
+import image1 from '../assets/OIP (2).jpg';
+import image2 from '../assets/OIP (6).jpg';
+import video1 from '../assets/hero.mp4';
 const Home = () => {
   return (
     <div>Home</div>
