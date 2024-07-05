365f578584579184ffb153c45c82bb6a855beb0c
Williyam Silvage Sachini Dishala <90495152+desi98@users.noreply.github.com>
2024-07-05 17:05:58 +0530

    add new images

--- src/components/Image_gallery/imageGallery.js
+++ src/components/Image_gallery/imageGallery.js
@@ -1,9 +1,24 @@
-import galleryImage1 from '../../assets/OIP (13).jpg'
-import galleryImage2 from '../../assets/OIP (11).jpg'
-import galleryImage3 from '../../assets/OIP (2).jpg'
-import galleryImage4 from '../../assets/OIP (7).jpg'
-import galleryImage5 from '../../assets/OIP (8).jpg'
-import galleryImage6 from '../../assets/OIP (3).jpg'
+import galleryImage1 from '../../assets/3c5c1bb999d7880a34c0afda3b0aa56c.jpg'
+import galleryImage2 from '../../assets/istockphoto-1152808475-612x612.jpg'
+import galleryImage3 from '../../assets/OIP (12).jpg'
+import galleryImage4 from '../../assets/OIP (14).jpg'
+import galleryImage5 from '../../assets/OIP (20).jpg'
+import galleryImage6 from '../../assets/OIP (15).jpg'
+import galleryImage7 from '../../assets/ambuluwawa.jpg'
+import galleryImage8 from '../../assets/OIP (21).jpg'
+import galleryImage9 from '../../assets/OIP (16).jpg'
+import galleryImage10 from '../../assets/OIP (22).jpg'
+import galleryImage11 from '../../assets/OIP (23).jpg'
+import galleryImage12 from '../../assets/OIP (17).jpg'
+import galleryImage13 from '../../assets/OIP (18).jpg'
+import galleryImage14 from '../../assets/OIP (19).jpg'
+import galleryImage15 from '../../assets/th.jpg'
+import galleryImage16 from '../../assets/cam.jpg'
+import galleryImage17 from '../../assets/w.jpg'
+import galleryImage18 from '../../assets/w2.jpg'
+import galleryImage19 from '../../assets/ad.jpg'
+import galleryImage20 from '../../assets/l.jpg'
+import galleryImage21 from '../../assets/we.jpg'
 
 
 const galleryImages =[
@@ -13,6 +28,24 @@ galleryImage3,
 galleryImage4,
 galleryImage5,
 galleryImage6,
+galleryImage7,
+galleryImage8,
+galleryImage9,
+galleryImage10,
+galleryImage11,
+galleryImage12,
+galleryImage13,
+galleryImage14,
+galleryImage15,
+galleryImage16,
+galleryImage17,
+galleryImage18,
+galleryImage19,
+galleryImage20,
+galleryImage21,
+
+
+
 ];
 
 export default galleryImages;
