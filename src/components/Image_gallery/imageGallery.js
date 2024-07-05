22a0f6067a1c314983317c586f86ee671cf96aae
Williyam Silvage Sachini Dishala <90495152+desi98@users.noreply.github.com>
2024-07-05 17:23:56 +0530

    do changes in picture order

--- src/components/Image_gallery/imageGallery.js
+++ src/components/Image_gallery/imageGallery.js
@@ -9,40 +9,45 @@ import galleryImage8 from '../../assets/OIP (21).jpg'
 import galleryImage9 from '../../assets/OIP (16).jpg'
 import galleryImage10 from '../../assets/OIP (22).jpg'
 import galleryImage11 from '../../assets/OIP (23).jpg'
-import galleryImage12 from '../../assets/OIP (17).jpg'
+
 import galleryImage13 from '../../assets/OIP (18).jpg'
-import galleryImage14 from '../../assets/OIP (19).jpg'
-import galleryImage15 from '../../assets/th.jpg'
-import galleryImage16 from '../../assets/cam.jpg'
-import galleryImage17 from '../../assets/w.jpg'
-import galleryImage18 from '../../assets/w2.jpg'
+import galleryImage14 from '../../assets/sigiriya.jpg'
+
+
+
+
 import galleryImage19 from '../../assets/ad.jpg'
 import galleryImage20 from '../../assets/l.jpg'
 import galleryImage21 from '../../assets/we.jpg'
+
 
 
 const galleryImages =[
 galleryImage1,
 galleryImage2,
 galleryImage3,
+
 galleryImage4,
 galleryImage5,
 galleryImage6,
 galleryImage7,
 galleryImage8,
 galleryImage9,
+galleryImage14,
 galleryImage10,
 galleryImage11,
-galleryImage12,
+
+
+
 galleryImage13,
-galleryImage14,
-galleryImage15,
-galleryImage16,
-galleryImage17,
-galleryImage18,
-galleryImage19,
-galleryImage20,
 galleryImage21,
+galleryImage20,
+galleryImage19,
+
+
+
+
+
 
 
 
