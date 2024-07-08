addf6ff9b50ba8c38ff4297a670a66ac809aa220
Williyam Silvage Sachini Dishala <90495152+desi98@users.noreply.github.com>
2024-07-08 11:48:51 +0530

    set icon in sutable sizes

--- src/Service/ServiceCard.jsx
+++ src/Service/ServiceCard.jsx
@@ -7,8 +7,8 @@ const{imgUrl,title,desc} =item
 
   return (
     <div className='service_item'>
-        <div className="service_img">
-            <img src={imgUrl} alt="" />
+        <div className="service_img ">
+            <img  src={imgUrl} alt="" />
         </div>
         <h5>{title}</h5>
         <p>{desc}</p>
