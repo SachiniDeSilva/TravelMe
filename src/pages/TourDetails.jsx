5ba7b36c5f6568f5e5e082517c166ab366c6d0c4
Williyam Silvage Sachini Dishala <90495152+desi98@users.noreply.github.com>
2024-07-11 23:43:48 +0530

    display tour details

--- src/pages/TourDetails.jsx
+++ src/pages/TourDetails.jsx
@@ -1,8 +1,40 @@
 import React from 'react'
-
+import '../styles/tour-details.css'
+import { Container, Row, Col, Form, ListGroup } from 'react-bootstrap'
+import { useParams } from 'react-router-dom'
+import tourData from '../assets/data/tours'
 const TourDetails = () => {
+
+
+
+  const {id } =useParams()
+
+
+
+  const tour = tourData.find(tour.id === id)//call api and data from db
+
+const {photo, title, desc, price, review, city, distance , maxGroupSize} = tour
+
   return (
-    <div>TourDetails</div>
+    <>
+    <section>
+      <Container>
+        <Row>
+          <Col lg='8'>
+          <div className="tour_content">
+            <img src={photo} alt="" />
+
+
+            <div className="tour_info">
+              <h2>{title}</h2>
+            </div>
+          </div>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+    
+    </>
   )
 }
 
